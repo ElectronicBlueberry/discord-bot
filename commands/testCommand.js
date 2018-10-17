2d2c2c1f5fea603fbe1ee04a11b5de3e45521a5f
Laila <mail@lailalos.com>
2018-10-18 00:50:55 +0200

    added another test command

--- commands/testCommand.js
+++ commands/testCommand.js
@@ -1,12 +1,26 @@
 const handler = require("../commandHandler.js");
 
-const testCommand = {
+handler.addCommand({
     name: "blub",
     role: "",
     run: (message, arguments) =>
     {
         message.channel.send("blaaaa!");
     }
-}
+});
 
-handler.addCommand(testCommand);
+handler.addCommand({
+    name: "add",
+    role: "mathematician",
+    run: (message, arguments) =>
+    {
+        if (arguments.length == 2)
+        {
+            message.channel.send(arguments[0] + " plus " + arguments[1] + " equals " + (parseInt(arguments[0]) + parseInt(arguments[1])) );
+        }
+        else
+        {
+            message.channel.send("I can only add two numbers! :frowning:");
+        }
+    }
+});
