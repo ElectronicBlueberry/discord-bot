dc907ecb8ccf5e5dc62783f310f359b477b73911
Laila <mail@lailalos.com>
2018-10-26 01:04:13 +0200

    fix

--- pluginLoader.js
+++ pluginLoader.js
@@ -4,87 +4,76 @@ const userdata = require("./userdata.js");
 
 // add role ids for all role elements
 userdata.client.on("ready", () => {	
-	let allCommands = [exports.channelCommands, exports.dmCommands];
+	let allCommands = [...exports.channelCommands, ...exports.dmCommands];
 
-	allCommands.forEach(e => {
-		e.forEach(command => {
-			command.roleId = clinet.guilds.first().roles.find(role => role.name === command.role);
-		});
+	allCommands.forEach(command => {
+		command.roleId = userdata.client.guilds.first().roles.find(role => role.name === command.role).id;
 	});
 });
 
-module.exports = {
+exports.channelCommands = [];
+exports.messageProcessors = [];
+exports.dmCommands = [];
+exports.reactionProcessors = [];
+exports.joinHandler = {};
 
-	scanPlugins: () => {
-		module.exports.channelCommands = [];
-		module.exports.messageProcessors = [];
-		module.exports.dmCommands = [];
-		module.exports.reactionProcessors = [];
-		module.exports.joinHandler = {};
+exports.scanPlugins = () => {
+	console.log("Searching for Plugins...");
 
-		console.log("Searching for Plugins...");
+	const files = fs.readdirSync(`./${config.plugin_folder}`);
+	let pluginList = [];
 
-		const files = fs.readdirSync(`./${config.plugin_folder}`);
-		let pluginList = [];
-
-		// Scan Folder for plugins
-		for (let i = 0; i < files.length; i++)
+	// Scan Folder for plugins
+	for (let i = 0; i < files.length; i++)
+	{
+		if (files[i].endsWith(config.plugin_suffix))
 		{
-			if (files[i].endsWith(config.plugin_suffix))
-			{
-				pluginList.push(files[i]);
-			}
+			pluginList.push(files[i]);
 		}
+	}
 
-		if (pluginList.length === 0)
-		{
-			return;
-		}
+	if (pluginList.length === 0)
+	{
+		return;
+	}
 
-		// Load all Plugins
-		for (let i = 0; i < pluginList.length; i++)
-		{
-			let content = fs.readFileSync(`./${config.plugin_folder}/${pluginList[i]}`);
-			let parameters = JSON.parse(content);
-
-			console.log("=======================");
-			console.log(`Loading Plugin "${parameters.name}"`);
-			console.log(`${parameters.log}`);
-
-			// Go through all files and load various command arrays
-			for (let a = 0; a < parameters.files.length; a++)
-			{
-				let script = require('require-reload')(`./${config.plugin_folder}/${parameters.files[a]}`);
-
-				if (script.channelCommands != undefined) {
-					module.exports.channelCommands = module.exports.channelCommands.concat(script.channelCommands);
-				}
-
-				if (script.dmCommands != undefined) {
-					module.exports.dmCommands = module.exports.dmCommands.concat(script.dmCommands);
-				}
-
-				if (script.messageProcessors != undefined) {
-					module.exports.messageProcessors = module.exports.messageProcessors.concat(script.messageProcessors);
-				}
-
-				if (script.dmCommands != undefined) {
-					module.exports.reactionProcessors = module.exports.reactionProcessors.concat(script.reactionProcessors);
-				}
-
-				if (script.joinHandler != undefined) {
-					module.exports.joinHandler = script.joinHandler;
-				}
-			}
-		}
+	// Load all Plugins
+	for (let i = 0; i < pluginList.length; i++)
+	{
+		let content = fs.readFileSync(`./${config.plugin_folder}/${pluginList[i]}`);
+		let parameters = JSON.parse(content);
 
 		console.log("=======================");
-		console.log(` ==== Sucessfully loaded ${pluginList.length} Plugins ==== `);
-	},
+		console.log(`Loading Plugin "${parameters.name}"`);
+		console.log(`${parameters.log}`);
 
-	channelCommands: [],
-	dmCommands: [],
-	messageProcessors: [],
-	reactionProcessors: [],
-	joinHandler: {}
+		// Go through all files and load various command arrays
+		for (let a = 0; a < parameters.files.length; a++)
+		{
+			let script = require('require-reload')(`./${config.plugin_folder}/${parameters.files[a]}`);
+
+			if (script.channelCommands != undefined) {
+				exports.channelCommands = exports.channelCommands.concat(script.channelCommands);
+			}
+
+			if (script.dmCommands != undefined) {
+				exports.dmCommands = exports.dmCommands.concat(script.dmCommands);
+			}
+
+			if (script.messageProcessors != undefined) {
+				exports.messageProcessors = exports.messageProcessors.concat(script.messageProcessors);
+			}
+
+			if (script.dmCommands != undefined) {
+				exports.reactionProcessors = exports.reactionProcessors.concat(script.reactionProcessors);
+			}
+
+			if (script.joinHandler != undefined) {
+				exports.joinHandler = script.joinHandler;
+			}
+		}
+	}
+
+	console.log("=======================");
+	console.log(` ==== Sucessfully loaded ${pluginList.length} Plugins ==== `);
 };
