f69803db718330768b80ecd0a1d5939c5d484be2
Laila <mail@lailalos.com>
2018-10-26 00:15:16 +0200

    minor changes

--- plugins/auto-leveling/wordCounter.js
+++ plugins/auto-leveling/wordCounter.js
@@ -1,4 +1,25 @@
 const userdata = require("../../userdata.js");
+const settings = require("./settings.json")
+
+var finalRole = settings.rank_ups[settings.rank_ups.length - 1].to;
+var MS_TO_MIN = 1 / 60000;
+
+async function promote(message, rankIndex, messageCount)
+{
+	if (messageCount < settings.rank_ups[rankIndex].message_count) {
+		return;
+	}
+
+	if ( (message.createdTimestamp - message.member.joinedTimestamp) * MS_TO_MIN < settings.rank_ups[rankIndex].time_count) {
+		return;
+	}
+
+	let roleIdTo   = message.guild.roles.find(r => r.name === settings.rank_ups[rankIndex].to  ).id;
+	let roleIdFrom = message.guild.roles.find(r => r.name === settings.rank_ups[rankIndex].from).id;
+
+	message.member.addRole   (roleIdTo  );
+	message.member.removeRole(roleIdFrom);
+}
 
 let countWords = {
 	name: "Wort Counter",
@@ -11,8 +32,17 @@ let countWords = {
 		}
 
 		messageCount++;
-
 		userdata.database.write(user, "messages", messageCount);
+
+		if (message.member.roles.find(r => r.name === finalRole) == undefined)
+		{
+			settings.rank_ups.forEach((role, i) => {
+				if (message.member.roles.find(r => r.name === role.from)) {
+					promote(message, i, messageCount);
+					return;
+				}
+			});
+		}
 	}
 };
 
