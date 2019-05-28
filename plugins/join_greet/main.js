e8053384f285fe1bd13d372e923fc1cdff7720fc
Laila <mail@lailalos.com>
2019-05-28 12:53:29 +0200

    new greeting plugin

---
+++ plugins/join_greet/main.js
@@ -0,0 +1,25 @@
+const settings = require('require-reload')('./settings.json');
+const userdata = require("../../userdata.js");
+
+function find_channel(channelName) {
+	let guild = userdata.client.guilds.first();
+	return guild.channels.find(channel => channel.name === channelName);
+}
+
+function build_message(user) {
+	let string = settings.greeting;
+	string = string.replace(/USERNAME/gm, `<@${user.id}>`);
+	string = string.replace(/CHANNEL#[^\s)\.,\/]*/gm, c => find_channel( c.substring( c.indexOf("#") + 1)).toString() );
+	return string;
+}
+
+var newUserJoin = {
+	run: (member) => {
+		let channel = find_channel(settings.channel);
+		channel.send( build_message(member));
+	}
+};
+
+module.exports = {
+	joinHandlers: [newUserJoin]
+};
