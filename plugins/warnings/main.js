6dfe79d01b5c791fb9597b2b913f901cce02954d
Laila <mail@lailalos.com>
2019-06-03 00:34:58 +0200

    new plugin

---
+++ plugins/warnings/main.js
@@ -0,0 +1,333 @@
+const settings = require('require-reload')('./settings.json');
+const userdata = require("../../userdata.js");
+const db = require("../../database/database.js")
+const texts = require('require-reload')('./texts.js');
+const config = require("../../config.json");
+
+// new database for warnigns without autosaves
+var database = new db.Database("warnings");
+
+function none_open(message, id) {
+	if (id == -1 || id == undefined) {
+		message.channel.send(texts.warning_none_open());
+		return true;
+	}
+
+	return false;
+}
+
+function reset_warning() {
+	database.write("global", "current_warning", -1);
+	database.write("meta", "vetos", []);
+	database.write("meta", "approves", []);
+	database.save();
+}
+
+function new_warning(message, arg, member) {
+	// unsent warning
+	let current = database.read("global", "current_warning");
+
+	if (current != -1 && current != undefined) {
+		message.channel.send(texts.warning_unsent());
+		return;
+	}
+
+	// wrong usage
+	if (!message.mentions.members.first()) {
+		message.channel.send(texts.warning_new_nouser());
+		return;
+	}
+
+	// get and increment ID
+	let count = database.read("global", "total_warning_count");
+	count = ++count || 0;
+
+	database.write("global", "current_warning", ++count || 0);
+	database.write("global", "total_warning_count", ++count || 0);
+
+	let author = member.id;
+	let target = message.mentions.members.first().id;
+	let level = parseInt(arg[2] || 1);
+
+	if (isNaN(level)) {
+		level = 1;
+	}
+
+	database.write("meta", "author", author);
+	database.write("meta", "target", target);
+	database.write("meta", "level", level);
+	database.save();
+
+	message.channel.send(texts.warning_new_sucess(author, target, level));
+}
+
+function set_content(message, member) {
+	let id = database.read("global", "current_warning");
+
+	if (none_open(message, id)) { return; }
+
+	if (id == database.read("content", "id")) {
+		message.channel.send(texts.warning_content_exists());
+		return;
+	}
+
+	if (member.id != database.read("meta", "author")) {
+		message.channel.send(texts.warning_wrong_author(database.read("meta", "author")));
+		return;
+	}
+
+	database.write("content", "message", message.id);
+	database.write("content", "id", id);
+	database.save();
+
+	message.channel.send(texts.warning_content_sucess());
+}
+
+function set_warning_level(message, target_level) {
+	if (none_open(message, database.read("global", "current_warning"))) { return; }
+
+	let level = parseInt(target_level);
+
+	if (isNaN(level)) {
+		level = 1;
+	}
+
+	database.write("meta", "level", level);
+	database.save();
+
+	message.channel.send(texts.warning_setlevel(level));
+}
+
+function cancel_warning(message) {
+	if (none_open(message, database.read("global", "current_warning"))) { return; }
+
+	reset_warning();
+
+	message.channel.send(texts.warning_cancel());
+}
+
+function decline_warning(message, member) {
+	if (none_open(message, database.read("global", "current_warning"))) { return; }
+
+	let vetos = database.read("meta", "vetos");
+	let approves = database.read("meta", "approves");
+	
+	if (vetos == undefined) {
+		vetos = [];
+	}
+
+	// Remove Approve
+	let approveIndex = approves.indexOf(member.id);
+	if (approveIndex != -1) {
+		approves.splice(approveIndex, 1);
+		database.write("meta", "approves", approves);
+	}
+
+	let vetoIndex = vetos.indexOf(member.id)
+	if (vetoIndex != -1) {
+		message.channel.send(texts.warning_declined_allready(member));
+		return;
+	}
+
+	vetos.push(member.id);
+
+	database.write("meta", "vetos", vetos);
+	database.save();
+
+	message.channel.send(texts.warning_declined(member));
+}
+
+function approve_warning(message, member) {
+	if (none_open(message, database.read("global", "current_warning"))) { return; }
+
+	let approves = database.read("meta", "approves");
+	let vetos = database.read("meta", "vetos");
+
+	if (approves == undefined) {
+		approves = [];
+	}
+
+	// Remove Veto
+	let vetoIndex = vetos.indexOf(member.id);
+	if (vetoIndex != -1) {
+		vetos.splice(vetoIndex, 1);
+		database.write("meta", "vetos", vetos);
+	}
+
+	let approveIndex = approves.indexOf(member.id);
+	if (approveIndex != -1) {
+		message.channel.send(texts.warning_approved_allready(member));
+		return;
+	}
+
+	approves.push(member.id);
+
+	database.write("meta", "approves", approves);
+	database.save();
+
+	message.channel.send(texts.warning_approved(member));
+}
+
+async function attempt_warning_send(message) {
+	let vetos = database.read("meta", "vetos");
+	let approves = database.read("meta", "approves");
+
+	if (none_open(message, database.read("global", "current_warning"))) { return; }
+
+	if (approves.length < settings.warning_approve_count) {
+		message.channel.send(texts.warning_insufficent_approves(approves.length));
+		return;
+	}
+	
+	if (vetos.length != 0) {
+		message.channel.send(texts.warning_vetoed(vetos));
+		return;
+	}
+
+	let content = await send_warning_to(message, database.read("meta", "target"));
+
+	if (content != null) {
+		message.channel.send(texts.warning_sent());
+		
+		let level = database.read("meta", "level");
+		let target = database.read("meta", "target");
+		let current_level = level_increase(target, parseInt(level));
+		
+		userdata.client.channels.find("name", settings.archive_channel).send(texts.warning_archive(
+			database.read("meta", "author"),
+			target,
+			content,
+			approves,
+			level,
+			current_level
+		));
+
+		reset_warning();
+	}
+}
+
+async function send_warning_to(message, target) {
+	let content_id = database.read("content", "id");
+	let id = database.read("global", "current_warning");
+
+	if (content_id != id) {
+		message.channel.send(texts.warning_no_content());
+		return null;
+	}
+
+	let message_id = database.read("content", "message");
+	let msg = await message.channel.fetchMessage(message_id);
+	let content = msg.content;
+	content = content.replace(`${config.prefix}${settings.command_warning} ${settings.command_warning_content}`,'');
+
+	let member = await userdata.client.fetchUser(target);
+	member.send(texts.warning_message(content));
+
+	return content;
+}
+
+function level_increase(target, value) {
+	let level = level_get(target) || 0;
+	level += value;
+
+	userdata.database.write(target, "warnlevel", level);
+	userdata.database.save();
+	return level;
+}
+
+function level_reset(target) {
+	userdata.database.write(target, "warnlevel", 0);
+	userdata.database.save();
+}
+
+function level_get(target) {
+	return userdata.database.read(target, "warnlevel");
+}
+
+var warning = {
+	name: settings.command_warning,
+	role: settings.role,
+	run: (message, arg, member) => {
+		if (arg.length === 0 || arg[0] == settings.command_help) {
+			message.channel.send(texts.warning_help());
+			return;
+		}
+
+		switch (arg[0]) {
+			case settings.command_warning_new:
+				new_warning(message, arg, member);
+				break;
+
+			case settings.command_warning_content:
+				set_content(message, member);
+				break;
+			
+			case settings.command_warning_cancel:
+				cancel_warning(message);
+				break;
+
+			case settings.command_warning_decline:
+				decline_warning(message, member);
+				break;
+
+			case settings.command_warning_approve:
+				approve_warning(message, member);
+				break;
+
+			case settings.command_warning_preview:
+				send_warning_to(message, member.id);
+				break;
+
+			case settings.command_warning_send:
+				attempt_warning_send(message);
+				break;
+		
+			case settings.command_warning_setlevel:
+				set_warning_level(message, arg[1]);
+				break;
+		}
+	}
+};
+
+var level = {
+	name: settings.command_level,
+	role: settings.role,
+	run: (message, arg) => {
+
+		if (!message.mentions.members.first()) {
+			if (!arg[0]) {
+				message.channel.send(texts.level_help());
+				return;
+			}
+
+			message.channel.send(texts.level_nouser());
+			return;
+		}
+
+		let target = message.mentions.members.first().id;
+
+		switch (arg[0]) {
+			case settings.command_level_reset:
+				level_reset(target);
+				message.channel.send(texts.level_reset(target));
+				break;
+
+			case settings.command_level_add:
+				let level = parseInt(arg[2] || 1);
+				if (isNaN(level)) {
+					level = 1;
+				}
+				level = level_increase(target, level);
+				message.channel.send(texts.level_increase(target, level));
+				break;
+
+			default:
+				message.channel.send(texts.level_get(target, level_get(target) || 0));
+				break;
+		}
+	}
+};
+
+module.exports = {
+	channelCommands: [warning, level]
+}
---
+++ plugins/warnings/texts.js
@@ -0,0 +1,196 @@
+const settings = require('require-reload')('./settings.json');
+const config = require("../../config.json");
+
+exports.warning_help = () => {
+	return `
+Nutze "${config.prefix}${settings.command_warning} ${settings.command_warning_new} @Nutzername Mahnstufe"
+um eine neue Mahnung zu erstellen.
+`;
+};
+
+exports.warning_unsent = () => {
+	return `
+Es kann keine neue Mahnung erstellt werden, solange es noch eine offene gibt!
+Versende die Mahnung mit "${config.prefix}${settings.command_warning} ${settings.command_warning_send}"
+oder breche die Mahnung mit "${config.prefix}${settings.command_warning} ${settings.command_warning_cancel}" ab.
+`;
+};
+
+exports.warning_new_nouser = () => {
+	return `
+Bitte nenne einen Nutzer in dem folgendem Format:
+"${config.prefix}${settings.command_warning} ${settings.command_warning_new} @Nutzername Mahnstufe"
+`;
+};
+
+exports.warning_new_sucess = (id, target, level) => {
+	return `
+Neue Mahnung erstellt von <@${id}> für Nutzer <@${target}> mit dem Mahnstufe ${level}
+
+Nutze jetzt
+"${config.prefix}${settings.command_warning} ${settings.command_warning_content} [Inhalt der Mahnung]"
+um die Mahnungsnachricht zu verfassen.
+`;
+};
+
+exports.warning_none_open = () => {
+	return `
+Keine offene Mahnung!
+
+Nutze "${config.prefix}${settings.command_warning} ${settings.command_warning_new} @Nutzername Mahnstufe"
+um eine Mahnung zu erstellen.
+`;
+};
+
+exports.warning_content_exists = () => {
+	return `
+Diese Mahnung hat bereits einen Inhalt.
+Editiere die Nachricht oder breche die Mahnung mit
+"${config.prefix}${settings.command_warning} ${settings.command_warning_cancel}"
+ab und starte eine neue.
+`;
+};
+
+exports.warning_content_sucess = () => {
+	return `
+Inhalt gesetzt.
+Editiere die Nachricht, um den Inhalt der Mahnung zu ändern.
+
+"${config.prefix}${settings.command_warning} ${settings.command_warning_approve}" um die Mahnung zu bestätigen.
+
+"${config.prefix}${settings.command_warning} ${settings.command_warning_decline}" um ein Veto einzulegen.
+
+"${config.prefix}${settings.command_warning} ${settings.command_warning_preview}" um eine Vorschau der Mahnung zu sehen.
+
+"${config.prefix}${settings.command_warning} ${settings.command_warning_send}" um die Mahnung zu senden.
+
+"${config.prefix}${settings.command_warning} ${settings.command_warning_setlevel} Mahnstufe" um die Mahnstufenerhöhung der Mahnung zu ändern.
+
+Es werden ${settings.warning_approve_count} Bestätigungen benötigt, bevor die Mahnung gesendet werden kann.
+`;
+};
+
+exports.warning_wrong_author = (author_id) => {
+	return `Nur <@${author_id}> kann den Inhalt dieser Mahnung festlegen`;
+};
+
+exports.warning_cancel = ()  => {
+	return `Mahnung abgebrochen!`;
+}
+
+exports.warning_declined = (member) => {
+	return `<@${member.id}> hat die Mahnung abgelehnt`;
+};
+
+exports.warning_declined_allready = (member) => {
+	return `<@${member.id}>, du hast diese Mahnung bereits abgelehnt!`;
+};
+
+exports.warning_approved = (member) => {
+	return `<@${member.id}> hat die Mahnung genehmigt`;
+};
+
+exports.warning_approved_allready = (member) => {
+	return `<@${member.id}>, du hast diese Mahnung bereits genehmigt!`;
+};
+
+exports.warning_vetoed = (vetos) => {
+	let veto_members = ``;
+	vetos.forEach(e => {veto_members += `<@${e}> \n`});
+
+	return `
+Die Mahnung konnte nicht gesendet werden, da sie von folgenden Personen abgelehnt wurde:
+
+${veto_members}
+Nutze "${config.prefix}${settings.command_warning} ${settings.command_warning_approve}",
+um eine Ablehnung zurückzuziehen
+`;
+};
+
+exports.warning_no_content = () => {
+	return `Die Aktuelle Mahnung hat keinen Inhalt oder es ist keine Mahnung offen`;
+};
+
+exports.warning_insufficent_approves = (count) => {
+	return `
+Nicht genug Bestätigungen: ${count} von ${settings.warning_approve_count}
+
+Nutze "${config.prefix}${settings.command_warning} ${settings.command_warning_approve}",
+um die Mahnung zu bestätigen
+`;
+};
+
+exports.warning_message = (content) => {
+	return `
+Du wurdest vom Modteam gemahnt.
+Inhalt der Mahnung:
+---------------------------
+${content}
+---------------------------
+Bitte nicht auf diese Nachricht antworten.
+`;
+};
+
+exports.warning_sent = () => {
+	return `Mahnung gesendet`;
+};
+
+exports.warning_archive = (author, target, content, approves, level, new_level) => {
+	let now = new Date();
+	let approve_members = ``;
+
+	approves.forEach(m => {approve_members += `<@${m}>\n`});
+
+	return `
+---  ${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}  ---
+
+Mahnung an: <@${target}>
+Mahnstufe: +${level}
+Gesamte Mahnstufe: ${new_level}
+
+Erstellt von: <@${author}>
+Genehmigt von:
+${approve_members}
+------ Inhalt ------
+
+${content}
+
+------ ------ ------
+`;
+};
+
+exports.warning_setlevel = (level) => {
+	return `Stufe der Mahnung auf ${level} geändert`;
+}
+
+exports.level_increase = (target, level) => {
+	return `Mahnstufe von <@${target}> auf ${level} erhöht`;
+};
+
+exports.level_reset = (target) => {
+	return `Mahnstufe von <@${target}> auf 0 zurückgesetzt`;
+};
+
+exports.level_get = (target, level) => {
+	return `Mahnstufe von <@${target}> ist ${level}`;
+};
+
+exports.level_help = () => {
+	return `
+Folgende Befehle sind für "${config.prefix}${settings.command_level}" verfügbar:
+
+"${config.prefix}${settings.command_level} @Nutzername" fragt die Mahnstufe ab
+
+"${config.prefix}${settings.command_level} ${settings.command_level_add} @Nutzername Mahnstufe" erhöht die Mahnstufe
+
+"${config.prefix}${settings.command_level} ${settings.command_level_reset} @Nutzername" setzt die Mahnstufe auf 0 zurück
+`;
+};
+
+exports.level_nouser = () => {
+	return `
+Nenne ein Mitglied. Beispiel:
+
+"${config.prefix}${settings.command_level} ${settings.command_level_add} @Nutzername Mahnstufe"
+`;
+};
