fcf8a83d237f39a6a705bb2e9e06a83424e811d2
Laila <mail@lailalos.com>
2019-06-03 14:36:13 +0200

    added functionality

--- utilities/searchForUser.js
+++ utilities/searchForUser.js
@@ -1,14 +1,21 @@
 exports.searchForUser = (message, userName) => {
 	let user;
-
+	
 	if (userName.includes('#')) {
-		user = message.guild.members.find(m => userName === m.user.tag);
-	} else {
-		user = message.guild.members.find(m => m.user.username === userName );
+		user = message.guild.members.find(m => userName == m.user.tag);
+	}
+	
+	if (user == undefined) {
+		user = message.guild.members.find(m => m.user.username == userName);
+	}
 
-		if (user == undefined){
-			user = message.guild.members.find(m => m.nickname === userName );
-		}
+	if (user == undefined){
+		user = message.guild.members.find(m => {return (m.nickname == userName || m.displayName == userName);} );
+	}
+
+	if (user == undefined && userName.includes('<@')) {
+		let userID = userName.replace(/<@|>/g, '');
+		user = message.guild.members.find(m => m.id == userID);
 	}
 
 	return user;
