1f8f24fd4118bfcd0c1255fd48292c751627a7bc
Laila <mail@lailalos.com>
2019-06-26 22:49:30 +0200

    autobann spammers

--- plugins/no_link/main.js
+++ plugins/no_link/main.js
@@ -3,19 +3,75 @@ const userdata = require("../../userdata.js");
 
 var role;
 
+function replaceUsername(message, string) {
+	return string.replace(/USERNAME/gm, `<@${message.member.id}>`);
+}
+
+function clearRespsone(message) {
+	setTimeout(m => m.delete(), settings.linger, message);
+}
+
+async function respond(message, string) {
+	string = replaceUsername(message, string);
+
+	try {
+		let deleted = await message.delete();
+		let response = await deleted.channel.send(string);
+		clearRespsone(response);
+	} catch {
+		console.error();
+	}
+	
+}
+
+function getTimer(id) {
+	return setTimeout((id) => {
+		spamData[id].count = 0;
+	}, settings.autobann.timeout, id);
+}
+
+// Data on all potentail message spams
+var spamData = {};
+
 var scanForUrl = {
 	run: (message) => {
 		if (/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gm.test(message.content)
 			&& !(message.member.roles.get(role.id)))
 		{
+			let id = message.member.id;
 
+			// If this is the users first link post attempt
+			if (!spamData[id] || spamData[id]["count"] == 0) {
+				spamData[id] = {count: 1};
+				
+				let timeout = getTimer(id);
+				spamData[id]["timeoutId"] = timeout;
 
-			let string = settings.message;
-			string = string.replace(/USERNAME/gm, `<@${message.member.id}>`);
+				// first warning message
+				respond(message, settings.message);
+			} else {
+				let count = spamData[id]["count"];
+				count += 1;
+				spamData[id]["count"] = count;
 
-			message.delete()
-				.then( msg => msg.channel.send(string) )
-				.catch( console.error );
+				// Remove old timer
+				clearTimeout(spamData[id]["timeoutId"]);
+
+				// Check if message limit is reached
+				// Not yet reached
+				if (count < settings.autobann.count) {		
+					// reset timer
+					spamData[id]["timeoutId"] = getTimer(id);
+
+					// warn
+					respond(message, settings.autobann.message);
+				} else {
+					// Ban, and delte messages from past Day
+					message.member.ban(1);
+					
+					delete spamData[id];
+				}
+			}
 		}
 	}
 };
